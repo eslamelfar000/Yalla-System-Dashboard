62c8873ee75f965a905479be727923f0be61e7b5
eslamelfar000 <eslamsaber707@gmail.com>
2025-04-22 00:01:41 +0200

    all

--- app/[lang]/(dashboard)/(apps)/income/page.jsx
+++ app/[lang]/(dashboard)/(apps)/income/page.jsx
@@ -1,7 +1,5 @@
 "use client";
 import React from "react";
-import PayBoxDataTable from "./paybox-table";
-import PayAfterDataTable from "./pay-after-tables";
 function page() {
   return <div>welcom</div>;
 }
