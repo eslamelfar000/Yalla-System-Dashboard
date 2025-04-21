3bb4e653375c2cfc63e6805dcecac93072e2e75f
eslamelfar000 <eslamsaber707@gmail.com>
2025-04-21 23:58:38 +0200

    all

--- app/[lang]/(dashboard)/(apps)/income/page.jsx
+++ app/[lang]/(dashboard)/(apps)/income/page.jsx
@@ -3,26 +3,7 @@ import React from "react";
 import PayBoxDataTable from "./paybox-table";
 import PayAfterDataTable from "./pay-after-tables";
 function page() {
-  return (
-    <div>
-      <div className="flex-1">
-        <h3 className="text-xl font-medium text-default-700 mb-2 opacity-60">
-          Pay After Sessions Students
-        </h3>
-      </div>
-      <PayBoxDataTable />
-
-      {/* .............. */}
-      <div className="mt-20">
-        <div className="flex-1">
-          <h3 className="text-xl font-medium text-default-700 mb-2 opacity-60">
-            Pay After Sessions Students
-          </h3>
-        </div>
-        <PayAfterDataTable />
-      </div>
-    </div>
-  );
+  return <div>welcom</div>;
 }
 
 export default page;
