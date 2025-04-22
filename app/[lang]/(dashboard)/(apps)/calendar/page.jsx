6d9547d39150146c0212a006684d43a8bbb367f0
eslamelfar000 <eslamsaber707@gmail.com>
2025-04-22 23:05:25 +0200

    last

--- app/[lang]/(dashboard)/(apps)/calendar/page.jsx
+++ app/[lang]/(dashboard)/(apps)/calendar/page.jsx
@@ -1,12 +1,19 @@
 import { getCategories, getEvents } from "@/config/calendar.config";
 import CalendarView from "./calender-view";
+import dynamic from "next/dynamic";
+
+const FCalendar = dynamic(() => import("./calender-view"), {
+  ssr: false,
+});
 
 const CalenderPage = async () => {
   const events = await getEvents();
   const categories = await getCategories();
   return (
     <div>
-      <CalendarView events={events?.data} categories={categories?.data} />
+      {/* <CalendarView events={events?.data} categories={categories?.data} /> */}
+      <br />
+      <FCalendar events={events?.data} categories={categories?.data} />
     </div>
   );
 };
