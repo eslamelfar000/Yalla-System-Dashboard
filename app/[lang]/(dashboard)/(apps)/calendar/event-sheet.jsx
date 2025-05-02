7920ed0abaf38df6f4f8c3e27fb2566ddaaa4732
eslamelfar000 <eslamsaber707@gmail.com>
2025-05-02 19:07:12 +0300

    again

--- app/[lang]/(dashboard)/(apps)/calendar/event-sheet.jsx
+++ app/[lang]/(dashboard)/(apps)/calendar/event-sheet.jsx
@@ -43,7 +43,7 @@ const EventSheet = ({ open, onClose, categories, event, selectedDate }) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [isPending, startTransition] = React.useTransition();
-  const [calendarProps, setCalendarProps] = React.useState(categories[0].value);
+  const [calendarProps, setCalendarProps] = React.useState(categories?.[0].value);
   // delete modal state
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const [eventIdToDelete, setEventIdToDelete] = useState(null);
@@ -271,7 +271,7 @@ const EventSheet = ({ open, onClose, categories, event, selectedDate }) => {
                               <SelectValue placeholder="Label" />
                             </SelectTrigger>
                             <SelectContent className="z-[9999]">
-                              {categories.map((category) => (
+                              {categories?.map((category) => (
                                 <SelectItem
                                   value={category.value}
                                   key={category.value}
